5f977820f46863fe9025e56f1e97c7780976a5e4
Aaron Krajeski <ak@aaronk.is>
2019-10-27 17:53:48 -0400

    Add loading shader

---
+++ shaders/loading.glsl
@@ -0,0 +1,39 @@
+#define NUMDOTS 14
+#define DOTOFFSET 0.04
+#define SPEED 0.2
+#define POWER 3.0
+#define topColor    vec3(0.3, 0.6, 1.0)
+#define bottomColor vec3(1.0, 0.4, 0.3)
+
+#define tau 6.283185
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+float smoothmin(float a, float b, float k)
+{
+    float h = clamp(0.5 + 0.5*(b-a)/k, 0.0, 1.0);
+    return mix(b, a, h) - k*h*(1.0-h);
+}
+
+void main()
+{
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy - 0.5;
+    uv.x *= u_resolution.x/u_resolution.y;
+
+    float d = 9999.0;
+    vec3 col;
+    for (int i = 0; i < NUMDOTS; i++) {
+      float t = pow(
+        fract(u_time * SPEED + (float(i) * DOTOFFSET)), POWER);
+      vec2 o = vec2(sin(t*tau), cos(t*tau)) * 0.2;
+        float dist = length(uv+o) - 0.01;
+        if (dist < d) {
+          col = mix(topColor, bottomColor, cos(t*tau)*0.5 + 0.5);
+        }
+    	d = smoothmin(d, dist, 0.04);
+    }
+    col = mix(col, vec3(1.0, 1.0, 0.9), smoothstep(0.0, 0.005, d));
+
+    gl_FragColor = vec4(col, 1.0);
+}
--- shaders/romanesco.glsl
+++ shaders/romanesco.glsl
@@ -4,39 +4,43 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-float t=5e-3;
+#define PI 3.14159
+
+float t = 5e-3;
 void main() {
   // Raymarching loop with a maximum of 64 steps
-  for( float i=0.;i<64.;i++) {
-    vec3 p=vec3((2.*gl_FragCoord.xy-u_resolution)/u_resolution.yy,t-1.),
-         b=vec3(.707,.707,0);
-    // Here we a apply a u_time dependent rotation around the Y-axis
-    float a=u_time;
-    p.xz*=mat2(cos(a),-sin(a),sin(a),cos(a));
+  for(float i = 0.0; i < 40.0; i++) {
+    vec3 p = vec3((2.0 * gl_FragCoord.xy-u_resolution) / u_resolution.yy, t - 1.0);
+    vec3 b = vec3(0.707, 0.707, 0);
+    // Here we a apply a time dependent rotation around the Y-axis
+    p.xz *= mat2(cos(u_time),-sin(u_time),sin(u_time),cos(u_time));
 
     // Fractal iteration loop
-    for( float i=0.;i<20.;i++) {
+    for(float i=0.0; i<20.0 ;i++) {
       // Rotating the space again around the Y- and Z-axis,
       // the angle depends on the u_mouse position this u_time
       // and ranges from 0 to (almost) 2π
-      a=(u_mouse/u_resolution*600.).x;
-      p.xz*=mat2(cos(a),-sin(a),sin(a),cos(a));
-      a=(u_mouse/u_resolution*600.).y;
-      p.xy*=mat2(cos(a),-sin(a),sin(a),cos(a));
+      vec2 m = (u_mouse * 2.0 * PI);
+      p.xz *= mat2(
+        cos(m.x), -sin(m.x),
+        sin(m.x),  cos(m.x));
+      p.xy *= mat2(
+        cos(m.y), -sin(m.y),
+        sin(m.y),  cos(m.y));
 
-      p-=min(0.,dot(p,b))*b*2.;
-      b=b.zxx;
-      p-=min(0.,dot(p,b))*b*2.;
-      b=b.zxz;
-      p-=min(0.,dot(p,b))*b*2.;
-      b=b.xxy;
-      p=p*1.5-.25;
+      p -= min(0.0, dot(p, b)) * b * 2.0;
+      b = b.zxx;
+      p -= min(0.0, dot(p, b)) * b * 2.0;
+      b = b.zxz;
+      p -= min(0.0, dot(p, b)) * b * 2.0;
+      b = b.xxy;
+      p = p * 1.5 - 0.25;
     }
-    t+=length(p)/3325.;
-    if(length(p)/3325.<5e-3||t>2.){
-      b=vec3(1);
-      p*=.5;
-      gl_FragColor=vec4(p/length(p)*(t<2.?5./i:i/64.),dot(p,b));
+    t += length(p) / 3325.0;
+    if(length(p)/3325.0<5e-3 || t > 2.0) {
+      b = vec3(1);
+      p *= 0.5;
+      gl_FragColor = vec4(p/length(p) * (t < 2.0 ? 5.0/i : i/64.0), dot(p, b));
       break;
     }
   }
