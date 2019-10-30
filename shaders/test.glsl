a2adfc101a96bef07fb7677b0a7b652947382e1a
Aaron Krajeski <ak@aaronk.is>
2019-10-30 12:49:20 -0400

    Load texture

--- shaders/test.glsl
+++ shaders/test.glsl
@@ -1,10 +1,14 @@
 precision mediump float;
 
+varying vec2 v_uv;
+
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
+uniform sampler2D u_mainTex;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy/u_resolution.xy;
-  gl_FragColor = vec4(uv, 1.0, 1.0);
+  vec2 uv = gl_FragCoord.xy / u_resolution;
+  vec3 tex = texture2D(u_mainTex, uv).rgb;
+  gl_FragColor = vec4(tex, 1.0);
 }
